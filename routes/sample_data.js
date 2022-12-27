5908594da3d55eeb2f6ccefdaaacb0fd64df925c
Lucas Kaczmarski <lucaskacz1@gmail.com>
2022-12-27 19:40:54 +0100

    Add Script

--- routes/sample_data.js
+++ routes/sample_data.js
@@ -37,36 +37,50 @@ router.get("/anomaly", function(request, response, next){
 	});
 });
 
-router.get('/query', function(request, response, next){
-    // Get the user input from the form submission
-    const userInput = req.query.userInput;
-  
-    // Select rows from the table based on the user input
-    database.query(`SELECT * FROM table WHERE column LIKE '%${userInput}%'`, (error, results) => {
-        if (error) throw error;
+router.get("/query", function(request, response, next){
 
-        // Generate the HTML table
-        let table = '<table>';
-        table += '<tr>';
-        // Add the table headers
-        for (const column in results[0]) {
-          table += `<th>${column}</th>`;
-        }
-        table += '</tr>';
-        // Add the table rows
-        for (const row of results) {
-          table += '<tr>';
-          for (const value in row) {
-            table += `<td>${row[value]}</td>`;
-          }
-          table += '</tr>';
-        }
-        table += '</table>';
-    
-        // Send the HTML table back to the client
-        // res.send(table);
-        response.render('sample_data', {title:'Price of the BTCUSDT Pair', action:'list', sampleData:results});
-      });
-    });
+  response.render("sample_data", {title:'Select data from MySQL', action:'add'});
+
+});
+  
+router.get("/result", function(request, response, next){
+
+  var selection = request.body.userinput;
+  var pair = document.getElementById('userinput').value;
+	var query = `SELECT * FROM anomaly;`;
+
+	database.query(query, function(error, data){
+
+		if(error)
+		{
+			throw error; 
+		}
+		else
+		{
+			response.render('sample_data', {title:'Anomalies in the BTCUSDT Pair', action:'ask', sampleData:data});
+		}
+	});
+});
+
+// router.post("/add_sample_data", function(request, response, next){
+
+//   var selection = request.body.value;
+
+//   var query = `SELECT * FROM btcusdt;`;
+
+//   database.query(query, function(error, data){
+
+//     if(error)
+//     {
+//       throw error;
+//     }	
+//     else
+//     {
+//       response.render('sample_data', {title:'Anomalies in the BTCUSDT Pair', action:'query', sampleData:data});
+//     }
+
+//   });
+
+// });
 
 module.exports = router;
